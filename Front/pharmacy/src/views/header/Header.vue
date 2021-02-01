9be2f3661cdf3186a719bf0daba44f36cf9f018a
ogikuzma <ognjenkuzmanovic1@gmail.com>
2021-02-01 18:39:06 +0100

    [feat]: Adding global header that could be easily extended

---
+++ Front/pharmacy/src/views/header/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">ISA Projekat</a>
+            </div>        
+            <SupplierHeader v-if="userRole == 'ROLE_SUPPLIER'" />
+            <PatientHeader v-else-if="userRole == 'ROLE_PATIENT'" />
+        </div>
+    </nav>
+</template>
+
+<script>
+import PatientHeader from "@/views/header/PatientHeader.vue";
+import SupplierHeader from "@/views/header/SupplierHeader.vue";
+
+export default {
+    data: function() {
+        return{
+            userRole: 'ROLE_UNREGISTERED'
+        }
+    },
+	components: {
+        PatientHeader,
+        SupplierHeader
+    },
+    mounted(){
+        this.userRole = this.$store.getters.getUserRole;
+    }
+};
+</script>
---
+++ Front/pharmacy/src/views/header/SupplierHeader.vue
@@ -0,0 +1,14 @@
+<template>    
+    <ul class="nav navbar-nav">
+        <li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Supplier 1</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Supplier 2</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Supplier 3</a></li>
+    </ul>
+</template>
+
+<style scoped>
+    .nav>li {
+        margin-left: 0.5rem;
+    }
+</style>
